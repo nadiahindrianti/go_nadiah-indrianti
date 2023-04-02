5e5cb0c64bb09d34f998cf7f1abb2a3c8f51b2d6
nadiahindrianti <104046022+nadiahindrianti@users.noreply.github.com>
2023-04-02 14:32:34 +0700

    Add files via upload

---
+++ 20_ORM & Code Structure (MVC)/Praktikum/lib/database/user.go
@@ -0,0 +1,15 @@
+package database
+
+import (
+	"ORM_MVC/config"
+	"ORM_MVC/models"
+)
+
+func GetUsers() (interface{}, error) {
+	var users []models.User
+
+	if e := config.DB.Find(&users).Error; e != nil {
+		return nil, e
+	}
+	return users, nil
+}
