4b00f52833c27bd8156546cc795ad141647188c6
nadiahindrianti <104046022+nadiahindrianti@users.noreply.github.com>
2023-04-05 01:22:06 +0700

    Update user.go

--- 21_Middleware/Praktikum/lib/database/user.go
+++ 21_Middleware/Praktikum/lib/database/user.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"ORM_MVC/config"
+	"ORM_MVC/middlewares"
 	"ORM_MVC/models"
 )
 
@@ -13,3 +14,30 @@ func GetUsers() (interface{}, error) {
 	}
 	return users, nil
 }
+
+func GetDetailUsers(userId int) (interface{}, error) {
+	var user models.User
+
+	if e := config.DB.Find(&user, userId).Error; e != nil {
+		return nil, e
+	}
+	return user, nil
+}
+
+func LoginUsers(user *models.User) (interface{}, error) {
+	var err error
+	if err = config.DB.Where("id = ? AND password = ?", user.ID, user.Password).First(&user).Error; err != nil {
+		return nil, err
+	}
+
+	user.Token, err = middlewares.CreateToken(int(user.ID))
+	if err != nil {
+		return nil, err
+	}
+
+	if err := config.DB.Save(user).Error; err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
