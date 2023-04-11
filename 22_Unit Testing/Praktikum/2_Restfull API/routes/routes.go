bd053f274f900998b47c48075a6a0f686ccd6d4c
nadiahindrianti <104046022+nadiahindrianti@users.noreply.github.com>
2023-04-11 22:53:21 +0700

    Add files via upload

---
+++ 22_Unit Testing/Praktikum/2_Restfull API/routes/routes.go
@@ -0,0 +1,54 @@
+package routes
+
+import (
+	"ORM_MVC/controllers"
+	m "ORM_MVC/middlewares"
+
+	"github.com/labstack/echo"
+	echoMid "github.com/labstack/echo/middleware"
+)
+
+func New() *echo.Echo {
+	// create a new echo instance
+	e := echo.New()
+	// Route / to handler function
+	m.LogMiddlewares(e)
+	e.GET("/users", controllers.GetUserController)
+	e.GET("/user/:id", controllers.GetUsersController)
+	e.POST("users", controllers.CreateUserController)
+	e.DELETE("users/:id", controllers.DeleteUserController)
+	e.PUT("/users/:id", controllers.UpdateUserController)
+	e.GET("/books", controllers.GetBooksController)
+	e.GET("/books/:id", controllers.GetBookController)
+	e.POST("books", controllers.CreateBookController)
+	e.DELETE("books/:id", controllers.DeleteBookController)
+	e.PUT("/books/:id", controllers.UpdateBookController)
+
+	eAuth := e.Group("/auth")
+	eAuth.Use(echoMid.BasicAuth(m.BasicAutoDB))
+	eAuth.GET("/users", controllers.GetUserController)
+	eAuth.GET("/user/:id", controllers.GetUsersController)
+	eAuth.POST("users", controllers.CreateUserController)
+	eAuth.POST("users/login", controllers.CreateUserController)
+	eAuth.DELETE("users/:id", controllers.DeleteUserController)
+	eAuth.PUT("/users/:id", controllers.UpdateUserController)
+	eAuth.GET("/books", controllers.GetBooksController)
+	eAuth.GET("/books/:id", controllers.GetBookController)
+	eAuth.POST("books", controllers.CreateBookController)
+	eAuth.DELETE("books/:id", controllers.DeleteBookController)
+	eAuth.PUT("/books/:id", controllers.UpdateBookController)
+
+	eJwt := e.Group("/jwt")
+	eJwt.GET("/users", controllers.GetUserDetailController)
+	eJwt.GET("/user/:id", controllers.GetUsersController)
+	eJwt.POST("users", controllers.CreateUserController)
+	eJwt.POST("users/login", controllers.LoginUserControllers)
+	eJwt.DELETE("users/:id", controllers.DeleteUserController)
+	eJwt.PUT("/users/:id", controllers.UpdateUserController)
+	eJwt.GET("/books", controllers.GetBooksController)
+	eJwt.GET("/books/:id", controllers.GetBookController)
+	eJwt.POST("books", controllers.CreateBookController)
+	eJwt.DELETE("books/:id", controllers.DeleteBookController)
+	eJwt.PUT("/books/:id", controllers.UpdateBookController)
+	return e
+}
